4476e9f6da109c6f4d23b17758df481549042ac4
Ish Takkar <ishtakkar21@gmail.com>
2021-05-29 14:39:18 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+// window.onscroll = function () { myFunction() };
+// var header = document.getElementById("header");
+// var sticky = header.offsetTop;
+// var search = document.querySelector('.navbar-search');
+
+// function myFunction() {
+//     if (window.pageYOffset > sticky) {
+//         header.classList.add("sticky");
+//         header.classList.add("color");
+//         search.classList.add("hide");
+//     } else {
+//         header.classList.remove("sticky");
+//         header.classList.remove("color");
+//         search.classList.remove("hide");
+//     }
+// }
